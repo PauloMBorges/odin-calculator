174de80e4c03788752f29898208dee0eec130881
PauloMBorges <paulodemeloborges@gmail.com>
2024-04-23 14:49:42 -0300

    Add operate() function
    This function takes 2 values - converts them using the Number() method if necessary - and an operator and then calls one of the basic four operations functions on the numbers.

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function divide(a, b) {
     if (b === 0) return "Math ERROR"
     return a / b;
 }
+
+function operate(a, b, op) {
+    a = Number(a);
+    b = Number(b);
+    switch(op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
